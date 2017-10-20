79af39e6c08e2d1fc6c8cd618ed99da4dae36c21
Melissa Cueto <melissa.cuetosilva@gmail.com>
2017-10-19 21:56:20 -0500

    "Enlace Melissa, holisx2"

--- src/conectionDB/cls_Conection.java
+++ src/conectionDB/cls_Conection.java
@@ -14,6 +14,7 @@ import java.sql.SQLException;
  * @author StoomBassCow
  */
 public class cls_Conection {
+    
     private static Connection conn;
     private static final String DRIVER = "com.mysql.jdbc.Driver";
     private static final String USER = "root";
@@ -40,7 +41,6 @@ public class cls_Conection {
         }
     }
 //Este metodo nos retorna la conexion
-
     public Connection getConnection() {
 
         return conn;
